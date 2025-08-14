668716e6859b875bf7c4dc6df163f034bebaa46a
didamendes <didamendes@hotmail.com>
2025-08-13 22:24:58 -0300

    Adicionando o Customer

---
+++ src/main/java/com/diogomendes/algashop/ordering/domain/entity/Customer.java
@@ -0,0 +1,26 @@
+package com.diogomendes.algashop.ordering.domain.entity;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+import java.time.OffsetDateTime;
+import java.util.UUID;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class Customer {
+    private UUID id;
+    private String fullName;
+    private LocalDate birthDate;
+    private String email;
+    private String phone;
+    private String document;
+    private Boolean promotionNotificationsAllowed;
+    private Boolean archived;
+    private OffsetDateTime registeredAt;
+    private OffsetDateTime archivedAt;
+    private Integer loyaltyPoints;
+}
